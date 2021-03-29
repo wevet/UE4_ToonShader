0ca184ab8344c8672aad786417b2b58108b4894a
wevet <waltzfor.floword@gmail.com>
2021-03-29 19:46:57 +0900

    initial commit

---
+++ Custom_ToonShade/bin/ClearCache.bat
@@ -0,0 +1,18 @@
+rmdir /q /s ..\\Intermediate
+rmdir /q /s ..\\Saved\\Autosaves
+rmdir /q /s ..\\Saved\\Backup
+rmdir /q /s ..\\Saved\\Collections
+rmdir /q /s ..\\Saved\\Config
+rmdir /q /s ..\\Saved\\Crashes
+rmdir /q /s ..\\Saved\\Logs
+
+rmdir /q /s ..\\.vs
+rmdir /q /s ..\\Binaries
+rmdir /q /s ..\\DerivedDataCache
+
+rmdir /q /s ..\\Plugins\\ImpostorBaker\\Binaries
+rmdir /q /s ..\\Plugins\\ImpostorBaker\\Intermediate
+
+
+pause
+exit 0
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # UE4_ToonShader
 UE4 Custom ToonShader
+Version 4.26
+
+
+# Includes Assets
+- Advanced locomotion system v3
+- UnityChan © UTJ/UCL
+- Imposter Baker
+
+# DEMO
+![MaterialInstance](Demo/Material_Instance.png)
+
+![Character](Demo/UnityChan_SRP.png)
